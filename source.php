05a4dad9bdc30e4f793ac763817e37c92719aec3
kame <robotbatbai@gmail.com>
2017-12-15 22:26:52 +0900

    edit title

--- source.php
+++ source.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <title>Bootstrap Example</title>
+      <title>Create Product Page</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -112,7 +112,6 @@
          <p id ="package_include">
          	<?php getTextAreaNewLine($_POST["package_include"]); ?>
          </p>
-
 	 </textarea>
       </div>
    </body>
